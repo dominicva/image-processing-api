80665c4710a37b213a0aa5217c77b295ff79545a
Dominic <dominicvana@gmail.com>
2021-05-24 05:53:05 +0100

    Chore: skeleton express server setup

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,16 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () =>
+  console.log(`Image Processing API server listening on port ${PORT}`)
+);
+
 const square = function (num: number): number {
   return num ** 2;
 };
