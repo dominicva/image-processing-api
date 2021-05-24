81e4944d3e1edb41aa3424ac69425d817fc935f2
Dominic <dominicvana@gmail.com>
2021-05-24 10:45:04 +0100

    Feat: writing resized imgg from query

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,9 @@ import fs from 'fs';
 import path from 'path';
 import express from 'express';
 import sharp from 'sharp';
+// const { pathToRegexp, match, parse, compile } = require('path-to-regexp');
+import url from 'url';
+import querystring from 'querystring';
 
 const app = express();
 const PORT = 3000;
@@ -22,19 +25,27 @@ const RESIZED_IMAGES_DIR = path.join(
   'resized'
 );
 
-async function semiTransparentRedPng() {
-  sharp(path.join(ORIGINAL_IMAGES_DIR, 'fjord.jpg'))
-    .resize(300, 200)
-    .toFile(path.join(RESIZED_IMAGES_DIR, 'output.jpg'), function (err) {
-      // output.jpg is a 300 pixels wide and 200 pixels high image
-      // containing a scaled and cropped version of input.jpg
-    });
-}
+const resizeAndWrite = function (
+  inputImg: string,
+  width: number,
+  height: number
+): void {
+  sharp(path.join(ORIGINAL_IMAGES_DIR, inputImg))
+    .resize(width, height)
+    .toFile(
+      path.join(RESIZED_IMAGES_DIR, `${inputImg}-w${width}h${height}.jpg`),
+      function (err) {
+        console.error(err);
+        // output.jpg is a 300 pixels wide and 200 pixels high image
+        // containing a scaled and cropped version of input.jpg
+      }
+    );
+};
 
-app.get('/api', async (req: express.Request, res: express.Response) => {
-  await semiTransparentRedPng();
-
-  res.send('hello worl');
+app.get('/api/images', (req: express.Request, res: express.Response): void => {
+  const { filename, width, height } = url.parse(req.url, true).query;
+  resizeAndWrite(String(filename), Number(width), Number(height));
+  res.send('hello world');
 });
 
 app.listen(PORT, () =>
