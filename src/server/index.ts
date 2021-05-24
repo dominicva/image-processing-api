69901ee0018c63a4ae1c1720ba0622ec62aa4aff
Dominic <dominicvana@gmail.com>
2021-05-24 06:27:21 +0100

    Feat: writing resized img  to resized dir

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,30 +6,30 @@ import sharp from 'sharp';
 const app = express();
 const PORT = 3000;
 
+const ORIGINAL_IMAGES_DIR = path.join(
+  __dirname,
+  '../',
+  '../',
+  'images',
+  'originals'
+);
+
+const RESIZED_IMAGES_DIR = path.join(
+  __dirname,
+  '../',
+  '../',
+  'images',
+  'resized'
+);
+
 async function semiTransparentRedPng() {
-  sharp(path.join(__dirname, '../', '../', 'images', 'originals', 'fjord.jpg'))
+  sharp(path.join(ORIGINAL_IMAGES_DIR, 'fjord.jpg'))
     .resize(300, 200)
-    .toFile('output.jpg', function (err) {
+    .toFile(path.join(RESIZED_IMAGES_DIR, 'output.jpg'), function (err) {
       // output.jpg is a 300 pixels wide and 200 pixels high image
       // containing a scaled and cropped version of input.jpg
     });
-  // return await sharp({
-  //   create: {
-  //     width: 48,
-  //     height: 48,
-  //     channels: 4,
-  //     background: { r: 255, g: 0, b: 0, alpha: 0.5 },
-  //   },
-  // })
-  //   .png()
-  //   .toBuffer();
 }
-
-// async function writeImageToFs() {
-//   const image = await semiTransparentRedPng();
-//   const imgPath = path.join(__dirname, '../', '../', 'images', 'resized');
-//   fs.writeFileSync(imgPath, image);
-// }
 
 app.get('/api', async (req: express.Request, res: express.Response) => {
   await semiTransparentRedPng();
