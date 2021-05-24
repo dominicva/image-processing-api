a898214e76e52803d341a292ac620142e2e862cc
Dominic <dominicvana@gmail.com>
2021-05-24 06:12:25 +0100

    Feat:: basic sharp response

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,27 @@
 import express from 'express';
+import sharp from 'sharp';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/api/:image', (req: express.Request, res: express.Response) => {
+async function semiTransparentRedPng() {
+  return await sharp({
+    create: {
+      width: 48,
+      height: 48,
+      channels: 4,
+      background: { r: 255, g: 0, b: 0, alpha: 0.5 },
+    },
+  })
+    .png()
+    .toBuffer();
+}
+
+app.get('/api/:image', async (req: express.Request, res: express.Response) => {
   const { image } = req.params;
   console.log(image);
-  res.send('Hello Word');
+  const result = await semiTransparentRedPng();
+  res.send(result);
 });
 
 app.listen(PORT, () =>
