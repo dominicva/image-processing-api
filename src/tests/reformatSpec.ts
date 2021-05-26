5196351211f36b6e2e946e92515a05b2058fc6ac
Dominic <dominicvana@gmail.com>
2021-05-26 08:05:17 +0100

    Feat: tests added for reformat

---
+++ src/tests/reformatSpec.ts
@@ -0,0 +1,16 @@
+import reformat from '../server/utils/reformat';
+
+const mocks = {
+  filepath:
+    '/Users/dominicvanalmsick/Downloads/code/udacity/fullstack-javascript-ND/image-processing-api/images/originals/palmtunnel.jpg',
+  formats: ['png', 'jpeg', 'webp', 'tiff'],
+};
+
+mocks.formats.forEach((format: string) => {
+  it(`expect reformat(mocks.filepath, ${format}) to return filepath with correct extension`, async () => {
+    const expectedResult = `/Users/dominicvanalmsick/Downloads/code/udacity/fullstack-javascript-ND/image-processing-api/images/processed/palmtunnel.${format}`;
+    const actualResult = await reformat(mocks.filepath, format);
+
+    expect(expectedResult).toEqual(String(actualResult));
+  });
+});
