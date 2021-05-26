5367719fdcd9781b74a7840897bcc3ee471435ee
Dominic <dominicvana@gmail.com>
2021-05-26 19:34:55 +0100

    Feat: init helper homepage

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,16 @@
+import path from 'path';
 import express from 'express';
 import processQuery from './routes/processQuery';
 
 const PORT = 3000;
 
 const app = express();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.sendFile(
+    path.join(__dirname, '../', '../', 'src', 'client', 'index.html')
+  );
+});
 
 app.get('/api/images', processQuery);
 
