4aa0faa9ae340f2d9232a7fa5d7d734d2df833b4
Dominic <dominicvana@gmail.com>
2021-05-27 08:06:30 +0100

    Feat: improve error handling

--- src/server/routes/processQuery.ts
+++ src/server/routes/processQuery.ts
@@ -9,30 +9,42 @@ const processQuery = async function (
   req: express.Request,
   res: express.Response
 ): Promise<void> {
-  const { filename, width, height, format, metadata, blurFactor, toGrayscale } =
-    req.query;
+  try {
+    const {
+      filename,
+      width,
+      height,
+      format,
+      metadata,
+      blurFactor,
+      toGrayscale,
+    } = req.query;
 
-  const inputFile = path.join(ORIGINAL_IMAGES_DIR, String(filename));
+    const inputFile = path.join(ORIGINAL_IMAGES_DIR, String(filename));
 
-  let outputFile: string | undefined;
+    let outputFile: string | undefined;
 
-  if (width && height) {
-    // resize
-    outputFile = await resize(inputFile, Number(width), Number(height));
-  } else if (format) {
-    // convert to specified format
-    outputFile = await reformat(inputFile, String(format));
-  } else if (metadata) {
-    // TODO: get meta data
-  } else if (blurFactor) {
-    outputFile = await blur(inputFile, Number(blurFactor));
-  } else if (toGrayscale) {
-    outputFile = await grayscale(inputFile);
-  }
+    if (width && height) {
+      // resize
+      outputFile = await resize(inputFile, Number(width), Number(height));
+    } else if (format) {
+      // convert to specified format
+      outputFile = await reformat(inputFile, String(format));
+    } else if (metadata) {
+      // TODO: get meta data
+    } else if (blurFactor) {
+      outputFile = await blur(inputFile, Number(blurFactor));
+    } else if (toGrayscale) {
+      outputFile = await grayscale(inputFile);
+    }
 
-  if (outputFile) {
-    res.sendFile(String(outputFile));
-  } else {
+    if (outputFile) {
+      res.sendFile(String(outputFile));
+    } else {
+      res.sendFile(path.join(CLIENT, 'index.html'));
+    }
+  } catch (err) {
+    console.error(err);
     res.sendFile(path.join(CLIENT, 'index.html'));
   }
 };
