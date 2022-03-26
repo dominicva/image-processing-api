84af41dda79d54903d691a3a9751bf5dc67c6111
Dominic <dominicvana@gmail.com>
2022-03-26 14:24:48 +0000

    Refactor: format conversions abstracted

--- src/server/utils/formatConversions.ts
+++ src/server/utils/formatConversions.ts
@@ -4,52 +4,35 @@ import { PROCESSED_IMAGES_DIR } from '../utils/dirPaths';
 import parsePath from './pathParser';
 import { inCache, storeInCache } from './cache';
 
-const toPng = async function (filepath: string): Promise<string> {
-  const image = parsePath.name(filepath);
-  const output = path.join(PROCESSED_IMAGES_DIR, `${image}.png`);
+function toFormat(format: string) {
+  return async function formatter(filepath: string): Promise<string> {
+    const image = parsePath.name(filepath);
+    const output = path.join(PROCESSED_IMAGES_DIR, `${image}.${format}`);
 
-  if (!inCache(output)) {
-    await sharp(filepath).png().toFile(output);
-    storeInCache(output);
-  }
+    if (!inCache(output)) {
+      switch (format) {
+        case 'png':
+          await sharp(filepath).png().toFile(output);
+          break;
+        case 'jpeg':
+          await sharp(filepath).jpeg().toFile(output);
+          break;
+        case 'webp':
+          await sharp(filepath).webp().toFile(output);
+          break;
+        case 'tiff':
+          await sharp(filepath).tiff().toFile(output);
+      }
+      storeInCache(output);
+    }
 
-  return output;
-};
+    return output;
+  };
+}
 
-const toJpeg = async function (filepath: string): Promise<string> {
-  const image = parsePath.name(filepath);
-  const output = path.join(PROCESSED_IMAGES_DIR, `${image}.jpeg`);
-
-  if (!inCache(output)) {
-    await sharp(filepath).jpeg().toFile(output);
-    storeInCache(output);
-  }
-
-  return output;
-};
-
-const toWebP = async function (filepath: string): Promise<string> {
-  const image = parsePath.name(filepath);
-  const output = path.join(PROCESSED_IMAGES_DIR, `${image}.webp`);
-
-  if (!inCache(output)) {
-    await sharp(filepath).webp().toFile(output);
-    storeInCache(output);
-  }
-
-  return output;
-};
-
-const toTiff = async function (filepath: string): Promise<string> {
-  const image = parsePath.name(filepath);
-  const output = path.join(PROCESSED_IMAGES_DIR, `${image}.tiff`);
-
-  if (!inCache(output)) {
-    await sharp(filepath).tiff().toFile(output);
-    storeInCache(output);
-  }
-
-  return output;
-};
+const toPng = toFormat('png');
+const toJpeg = toFormat('jpeg');
+const toWebP = toFormat('webp');
+const toTiff = toFormat('tiff');
 
 export { toPng, toJpeg, toWebP, toTiff };
